49a00582f45da02713e8315831aff4a647bb10a6
Patrik Krajňák <patrik.krajnak@student.tuke.sk>
2024-03-15 10:19:20 +0100

    added pwa to project

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import './registerServiceWorker'
 
 const vuetify = createVuetify({
   components,

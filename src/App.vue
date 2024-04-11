240083c3b540f12f94c051d7cd7e4704ffefa13a
Patrik Krajňák <patrik.krajnak@student.tuke.sk>
2024-04-11 18:20:40 +0200

    trying to setup hotjar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+  <!-- Hotjar Tracking Code for https://tukecrowdfunding.web.app/ -->
+  <script>
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:4940472,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
   <v-container>
     <v-toolbar color="white">
       
